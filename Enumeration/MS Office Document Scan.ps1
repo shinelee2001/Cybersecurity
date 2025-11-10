a9b25ad4368db0f594c119519d1df599ce4fd677
shinelee2001 <77053233+shinelee2001@users.noreply.github.com>
2025-11-10 13:29:53 -0500

    Bug fixes

--- Enumeration/MS Office Document Scan.ps1
+++ Enumeration/MS Office Document Scan.ps1
@@ -6,141 +6,184 @@ $keywordPtn = "(?i:$keywordPtn)"
 
 $keywordFoundFiles = New-Object System.Collections.Generic.List[object]
 
-# Helper for DOXC
-function Get-DoxcText {
-	param([string]$Path)
-
-    # Temp file and folder to unzip the document
-    $tmpZip = [System.IO.Path]::ChangeExtension($Path, ".zip")
-    $tmpFolder = Join-Path ([System.IO.Path]::GetDirectoryName($Path)) "tmp_docx"
-
-    # Check if the temp file or folder exists and remove them before unzip the current document
-    if (Test-Path -LiteralPath $tmpFolder) {Remove-Item -Recurse -Force $tmpFolder}
-    if (Test-Path -LiteralPath $tmpZip) {Remove-Item -Force $tmpZip}
-
-    # Unzip the document in temp folder
-    Copy-Item $Path $tmpZip
-    Expand-Archive -Path $tmpZip -DestinationPath $tmpFolder -Force
-
-    $documentXml = Join-Path $tmpFolder "word\document.xml"
-    [xml]$xml = Get-Content $documentXml
-    $textNodes = $xml.GetElementsByTagName("w:t")
-    $text = ($textNodes | ForEach-Object{$_.'#text'}) -join ' '
-    $lines = $text -split '\. '
-
-    # Look up for the keywords
-    $matched = $lines | Where-Object {$_ -match $keywordPtn}
-
-    # Remove temp file and folder
-    Remove-Item -Recurse -Force $tmpFolder
-    Remove-Item -Force $tmpZip
-
-    if (-not ($matched)) {return}
-    return $text
-}
-
-# Helper for PPTX
-function Get-PptxText {
+# Helper to extract texts from DOCX.
+function Get-DocxText {
     param([string]$Path)
-
-    # Temp file and folder to unzip document
-    $tmpZip = [System.IO.Path]::ChangeExtension($Path, ".zip")
-    $tmpFolder = Join-Path [System.IO.Path]::GetDirectoryName($Path) "tmp_pptx"
-
-    # Check if the temp file or folder exists and remove them before unzip the current document
-    if (Test-Path -LiteralPath $tmpFolder) {Remove-Item -Recurse -Force $tmpFolder}
-    if (Test-Path -LiteralPath $tmpZip) {Remove-Item -Force $tmpZip}
-
-    # Unzip the document in temp folder
-    Copy-Item $Path $tmpZip
-    Expand-Archive -Path $tmpZip -DestinationPath $tmpFolder -Force
-
-    $slideDir = Join-Path $tmpFolder "ppt\slides"
-    $slideFiles = Get-ChildItem $slideDir -Filter "slide*.xml" -File
-
-    $slideTexts = @{}
-    foreach($sf in $slideFiles){
-        [xml]$xml = Get-Content $sf.FullName
-        $textNodes = $xml.SelectNodes('//*[local-name()="t"]')
-        $text = ($textNodes | ForEach-Object{$_.InnerText}) -join ' '
+	
+	# Temp file and folder to unzip the document.
+	$tmpZip = [System.IO.Path]::ChangeExtension($Path, ".zip")
+	$tmpFolder = Join-Path ([System.IO.Path]::GetDirectoryName($Path)) "tmp_docx"
+	
+	# Check if the temp file or folder exists and remove them before unzip the current document.
+	if (Test-Path -LiteralPath $tmpFolder) { Remove-Item -Recurse -Force $tmpFolder }
+    if (Test-Path -LiteralPath $tmpZip) { Remove-Item -Force $tmpZip }
+	
+	# Unzip the document in temp folder
+	Copy-Item $Path $tmpZip
+	Expand-Archive -Path $tmpZip -DestinationPath $tmpFolder -Force
+	
+	
+	# (2025-11-05) Scanning logic fix for docx
+	# 	- Now it scans through headers and footers as well.
+	$fileNames = @(Join-Path $tmpFolder 'word\document.xml') + 
+		(Get-ChildItem -LiteralPath (Join-Path $tmpFolder 'word') -Filter 'header*.xml' -ErrorAction SilentlyContinue |
+			ForEach-Object { $_.FullName }) +
+		(Get-ChildItem -LiteralPath (Join-Path $tmpFolder 'word') -Filter 'footer*.xml' -ErrorAction SilentlyContinue |
+			ForEach-Object { $_.FullName })
+	$docxTexts = @{}
+	
+	foreach($docxFile in $fileNames){
+		[xml]$xml = Get-Content -LiteralPath $docxFile -Raw
+		
+		$textNodes = $xml.SelectNodes('//*[local-name()="t"]')
+        $text = ($textNodes | ForEach-Object { $_.InnerText }) -join ' '
         $lines = $text -split '\. '
 
-        # Look up for the keywords
-        $matched = $lines | Where-Object{$_ -match $keywordPtn}
-        if ($matched){$slideTexts[$sf.Name] = ($matched) -join "`n"}
-    }
 
-    # Remove temp file and folder
-    Remove-Item -Recurse -Force $tmpFolder
+        $matched = $lines | Where-Object { $_ -and ($_ -match $keywordPtn) }
+		if ($matched) {$docxTexts[($docxFile -split '\\')[-1]] = ($matched -join "`n")}
+	}
+	
+	# Remove temp file and folder
+	Remove-Item -Recurse -Force $tmpFolder
     Remove-Item -Force $tmpZip
 
-    return $slideTexts
+    return $docxTexts
 }
 
-# Helper for XLSX
-function Get-XlsxText {
+
+# Helper to extract texts from PPTX.
+function Get-PptxSlidesText {
     param([string]$Path)
 
-    # Temp file and folder to unzip document
-    $tmpZip = [System.IO.Path]::ChangeExtension($Path, ".zip")
-    $tmpFolder = Join-Path [System.IO.Path]::GetDirectoryName($Path) "tmp_xlsx"
+	# Temp file and folder to unzip the document.
+    $tmpZip    = [System.IO.Path]::ChangeExtension($Path, ".zip")
+    $tmpFolder = Join-Path ([System.IO.Path]::GetDirectoryName($Path)) "temp_pptx"
 
-    # Check if the temp file or folder exists and remove them before unzip the current document
-    if (Test-Path -LiteralPath $tmpFolder) {Remove-Item -Recurse -Force $tmpFolder}
-    if (Test-Path -LiteralPath $tmpZip) {Remove-Item -Force $tmpZip}
+	# Check if the temp file or folder exists and remove them before unzip the current document.
+    if (Test-Path -LiteralPath $tmpFolder) { Remove-Item -Recurse -Force $tmpFolder }
+    if (Test-Path -LiteralPath $tmpZip)    { Remove-Item -Force $tmpZip }
 
-    # Unzip the document in temp folder
-    Copy-Item $Path $tmpZip
-    Expand-Archive -Path $tmpZip -DestinationPath $tmpFolder -Force
-
-    $excelTexts = @{}
-
-    # 1) sharedStrings.xml
-    $sharedStringsXml = Join-Path $tmpFolder "xl\sharedStrings.xml"
-    [xml]$ssXml = Get-Content $sharedStringsXml
-    $ssTextNodes = $ssXml.SelectNodes('//*[local-name()="t"]')
-    $ssText = ($ssTextNodes | ForEach-Object{$_.InnerText}) -join ' '
-    $ssLines = $ssText -split '\. '
-
-    $ssMatched = $ssLines | Where-Object{$_ -match $keywordPtn}
-    if ($ssMatched) {$excelTexts["sharedStrings.xml"]=($ssMatched -join "`n")}
-
-    # 2) drawing*.xml
-    $drawingDir = Join-Path $tmpFolder "xl\drawings"
-    $drawingFiles = Get-ChildItem $drawingDir -Filter "drawing*.xml" -File
-    foreach($df in $drawingFiles){
-        [xml]$dfXml = Get-Content $df.FullName
-        $dfTextNodes = $dfXml.SelectNodes('//*[local-name()="t"]')
-        $dfText = ($dfTextNodes|ForEach-Object{$_.InnerText}) -join ' '
-        $dfLines = $dfText -split '\. '
-
-        $dfMatched = $dfLines | Where-Object{$_ -match $keywordPtn}
-        if ($dfMatched){$excelTexts[$df.Name]=($dfMatched -join "`n")}
-    }
-
-    # 3) chartEx*.xml
-    $chartDir = Join-Path $tmpFolder "xl\charts"
-    $chartFiles = Get-ChildItem $chartDir -Filter "chart*.xml" -File
-    foreach($cf in $chartFiles){
-        [xml]$cfXml = Get-Content $cf.FullName
-        $cfTextNodes = $cfXml.SelectNodes(
-            '//*[local-name()="t"]',
-            '//*[local-name()="v"]',
-            '//*[local-name()="tx"]',
-            '//*[local-name()="rich"]',
-            '//*[local-name()="title"]',
-            '//*[local-name()="legend"]'
-            )
-        $cfText = ($cfTextNodes|ForEach-Object{$_.InnerText}) -join ' '
-        $cfLines = $cfText -split '\. '
-
-        $cfMatched = $cfLines | Where-Object{$_ -match $keywordPtn}
-        if ($cfMatched){$excelTexts[$cf.Name]=($cfMatched -join "`n")}
-    }
-
-    # Remove temp file and folder
-    Remove-Item -Recurse -Force $tmpFolder
+	# Unzip the document in temp folder
+	Copy-Item $Path $tmpZip -Force
+	Expand-Archive -Path $tmpZip -DestinationPath $tmpFolder -Force
+	
+	$slidesDir = Join-Path $tmpFolder "ppt\slides"
+	if (-not (Test-Path -LiteralPath $slidesDir)) {return}
+	$slideFiles = Get-ChildItem -LiteralPath $slidesDir -Filter "slide*.xml" -File
+	
+	$slideTexts = @{}
+	
+	
+	foreach($sf in $slideFiles) {
+		[xml]$xml = Get-Content $sf.FullName -Raw
+		
+		$tNodes = $xml.SelectNodes('//*[local-name()="t" and namespace-uri()="http://schemas.openxmlformats.org/drawingml/2006/main"]')
+		
+		$text = ($tNodes | ForEach-Object { $_.InnerText }) -join ' '
+		$lines = $text -split '\. '
+		
+		# Look up for the keywords in the document
+		$matched = $lines | Where-Object { $_ -match $keywordPtn }
+		if ($matched) {$slideTexts[$sf.Name] = ($matched -join "`n")}
+		
+	}
+	
+	# Remove temp file and folder.
+	Remove-Item -Recurse -Force $tmpFolder
     Remove-Item -Force $tmpZip
+	
+	return $slideTexts
+}
 
-    return $excelTexts
+# Helper to extract texts from Excel.
+function Get-ExcelText {
+    param([string]$Path)
+
+	# Temp file and folder to unzip the document.
+    $tmpZip    = [System.IO.Path]::ChangeExtension($Path, ".zip")
+    $tmpFolder = Join-Path ([System.IO.Path]::GetDirectoryName($Path)) "temp_xlsx"
+
+	# Check if the temp file or folder exists and remove them before unzip the current document.
+    if (Test-Path -LiteralPath $tmpFolder) { Remove-Item -Recurse -Force $tmpFolder }
+    if (Test-Path -LiteralPath $tmpZip)    { Remove-Item -Force $tmpZip }
+
+	# Unzip the document in temp folder
+	Copy-Item $Path $tmpZip -Force
+	Expand-Archive -Path $tmpZip -DestinationPath $tmpFolder -Force
+	
+	
+	$excelTexts = @{}
+	
+	# 1) sharedStrings.xml
+	$sharedStrings = Join-Path $tmpFolder "xl\sharedStrings.xml"
+	if (-not (Test-Path -LiteralPath $sharedStrings)) {
+		# Write-Warning "xl\sharedStrings.xml not found in $Path"
+		return
+	}
+	[xml]$ssXml = [xml]$ssXml = Get-Content -LiteralPath $sharedStrings -Raw -Encoding UTF8
+	$ssTNodes = $ssXml.SelectNodes('//*[local-name()="t"]')
+	$ssText = ($ssTNodes | ForEach-Object { $_.InnerText }) -join ' '
+	$ssLines = $ssText -split '\. '
+	
+	# Look up for the keywords in the document
+	$ssMatched = $ssLines | Where-Object { $_ -match $keywordPtn}
+	if ($ssMatched) {$excelTexts["sharedStrings.xml"] = ($ssMatched -join "`n") }
+	
+	
+	
+	# 2) drawing*.xml
+	$drawingDir = Join-Path $tmpFolder "xl\drawings"
+	if (-not (Test-Path -LiteralPath $drawingDir)) {return}
+	$drawingFiles = Get-ChildItem -LiteralPath $drawingDir -Filter "drawing*.xml" -File
+	
+	foreach($df in $drawingFiles) {
+		[xml]$xml = Get-Content $df.FullName -Raw -Encoding UTF8
+		
+		$tNodes = $xml.SelectNodes('//*[local-name()="t"]')
+		
+		$text = ($tNodes | ForEach-Object { $_.InnerText }) -join ' '
+		$lines = $text -split '\. '
+		
+		# Look up for the keywords in the document
+		$matched = $lines | Where-Object { $_ -match $keywordPtn }
+		if ($matched) {$excelTexts[$df.Name] = ($matched -join "`n")}
+	}
+	
+	# 3) chartEx*.xml
+	$chartDir = Join-Path $tmpFolder "xl\charts"
+	if (-not (Test-Path -LiteralPath $chartDir)) {return}
+	
+	$chartFiles = Get-ChildItem -LiteralPath $chartDir -Filter "chart*.xml" -File
+	foreach($cf in $chartFiles) {
+		[xml]$xml = Get-Content $cf.FullName -Raw -Encoding UTF8
+		
+		$paths = @(
+			'//*[local-name()="t" and namespace-uri()="http://schemas.openxmlformats.org/drawingml/2006/main"]',
+            '//*[local-name()="v" and namespace-uri()="http://schemas.openxmlformats.org/drawingml/2006/chart"]',
+            '//*[local-name()="tx"]//*[local-name()="t"]',
+            '//*[local-name()="rich"]//*[local-name()="t"]',
+            '//*[local-name()="title"]//*[local-name()="t"]',
+            '//*[local-name()="legend"]//*[local-name()="t"]'
+		)
+		foreach ($path in $paths) {
+			$nodes = $xml.SelectNodes($path)
+			if ($nodes) {
+				foreach($n in $nodes) {
+					$chartText = $($n.InnerText) -join ' '
+					$chartLines = $chartText -split '\. '
+					
+					# Look up for the keywords in the document
+					$chartMatched = $chartLines | Where-Object {$_ -match $keywordPtn}
+					if ($chartMatched) {$excelTexts[$cf.Name] = ($chartMatched -join "`n")}
+				}
+			}
+		}
+	}
+	
+	# Remove temp file and folder.
+	Remove-Item -Recurse -Force $tmpFolder
+    Remove-Item -Force $tmpZip
+	
+	return $excelTexts
 }
