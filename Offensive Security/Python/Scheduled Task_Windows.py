7ef1caafa9d9e98ed3604dcdb2ffaa6882ef1112
Dongchan Lee <shinelee2001@gmail.com>
2025-11-12 22:11:56 -0500

    Python update

---
+++ Offensive Security/Python/Scheduled Task_Windows.py
@@ -0,0 +1,20 @@
+# schtasks
+
+import os, random
+from datetime import datetime, timedelta
+
+if os.system("schtasks /query /tn /SecurityScan") == 0:
+    os.system("schtasks /delete /f /tn /SecurityScan")
+
+print("Malicious thing is happening")
+# --- Do some malicious things here ---
+
+filedir = os.path.join(os.getcwd(),"sched.py")
+
+maxInterval = 1
+interval = 1+(random.random() * (maxinterval-1))
+dt = datetime.now() + timedelta(munites=interval)
+t="%s:%s" % (str(dt.hour).zfill(2), str(dt.minute).zfill(2))
+d="%s/%s/%s" % (dt.month, str(dt.day).zfill(2), dt.year)
+os.system('schtasks /create /tn SecurityScan /tr "'+filedir+'" /sc once /st '+t+' /sd '+d)
+input()
